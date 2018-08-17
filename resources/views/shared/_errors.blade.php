558d1f2026ca59257780877042ae129c126b13c2
company cscroy <cciipq1@gmail.com>
2018-08-17 10:00:30 +0000

    bak

---
+++ resources/views/shared/_errors.blade.php
@@ -0,0 +1,13 @@
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+
+@endforeach
+
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
