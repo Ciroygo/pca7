88f7803d2e1951fb68e810d3d16abd5392bd31e0
company cscroy <cciipq1@gmail.com>
2018-09-21 09:45:26 +0000

    fix bug

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,8 +78,8 @@ class UsersController extends Controller
         $to = $user->email;
         $subject = "感谢注册ciroy 应用！请确认邮箱";
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject){
+            $message->to($to)->subject($subject);
         });
     }
 
